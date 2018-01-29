50e266820510067eb74f4c95a35a9a20d3a2d006
Evgeniy Talko <etalko@gmail.com>
2018-01-29 20:20:34 +0300

    fix issue when defaul profile applies only on exit from program in profile-list

--- wacom-profile-switcher.sh
+++ wacom-profile-switcher.sh
@@ -3,42 +3,34 @@ awk_string1="/"
 awk_string2="/{print \$1}"
 window_in_focus_old=""
 
-while true
-do
-  #get ID of windows in the focus
-  window_in_focus=`xprop -root |awk '/_NET_ACTIVE_WINDOW/ {print $5; exit;}'`
-  figma=`xdotool getwindowfocus getwindowname | tr -d '[:space:]''[="=]' | sed -r 's/(–|-)/ /g' | cut -f 2 -d " "`
-  echo $figma
-  #if the focus didn't change - do nothing
-  if [[ "$window_in_focus" -ne "$window_in_focus_old" ]]
-   then
-   	  window_in_focus_old=$window_in_focus
-  	  #reading program's name from file "programs"
-      list_of_progr=$(<$HOME/wacom-profile-switcher/programs)	
-	  # program's names cycle
-	  for name_of_progr in $list_of_progr
-	  do
-	  	awk_string_result="$awk_string1$name_of_progr$awk_string2"
-		# getting the list of windows, wich titles has keyword from list of programs
-	  	list_of_running_prog=`wmctrl -l | awk "${awk_string_result}"`
-	  	# if in focus - then will execute script in the xsetwacom directory
-	  	for running_progr in $list_of_running_prog 
-	  	do
-	  		if [[ "$running_progr" -eq "$window_in_focus" ]] 
-	  		 then
-            #Placeholder for figma condition
-            ## elif [[ "$figma" -eq "Figma" ]]
-            ## $HOME/wacom-profile-switcher/figma
-	  			$HOME/wacom-profile-switcher/$name_of_progr
-             else
-                $HOME/wacom-profile-switcher/default
-	  			
-	  		fi
-	  	done
-	  done
-  fi  
-
-sleep 1
+while true; do
+	#get ID of windows in the focus
+	window_in_focus=$(xprop -root | awk '/_NET_ACTIVE_WINDOW/ {print $5; exit;}')
+	figma=$(xdotool getwindowfocus getwindowname | tr -d '[:space:]''[="=]' | sed -r 's/(–|-)/ /g' | cut -f 2 -d " ")
+	# echo $figma
+	#if the focus didn't change - do nothing
+	if [[ "$window_in_focus" -ne "$window_in_focus_old" ]]; then
+		window_in_focus_old=$window_in_focus
+		#reading program's name from file "programs"
+		list_of_progr=$(<$HOME/wacom-profile-switcher/programs)
+		# program's names cycle
+		for name_of_progr in $list_of_progr; do
+			awk_string_result="$awk_string1$name_of_progr$awk_string2"
+			# getting the list of windows, wich titles has keyword from list of programs
+			list_of_running_prog=$(wmctrl -l | awk "${awk_string_result}")
+			# if in focus - then will execute script in the xsetwacom directory
+			for running_progr in $list_of_running_prog; do
+				if [[ "$running_progr" -ne "$window_in_focus" ]]; then
+					$HOME/wacom-profile-switcher/default
+				elif [[ "$running_progr" -eq "$window_in_focus" ]]; then
+					$HOME/wacom-profile-switcher/$name_of_progr
+				elif [[ "$figma" -eq "Figma" ]]; then
+					$HOME/wacom-profile-switcher/figma
+				fi
+			done
+		done
+	fi
+	sleep 1
 done
 
 ## Get window name. Assign to variable
